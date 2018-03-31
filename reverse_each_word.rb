7b4b0b0547042c04262420479ad7080c1c4a05be
Monica Marziano <mmarziano1@gmail.com>
2018-03-31 11:57:33 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
-  sentence.split(", ")
-  sentence.each do |word|
-    word.reverse 
+  sentence_array = sentence.split(", ") 
+  sentence_array.each do |words|
+    return words.reverse
+  end
   end
   
 end
